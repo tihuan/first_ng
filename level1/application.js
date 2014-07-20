ad9fc132cee5c52e0470b02855fe8857cc6d7c65
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-20 15:50:43 -0700

    Add ng-submit to form

--- level1/application.js
+++ level1/application.js
@@ -20,6 +20,9 @@
   });
   app.controller('ReviewController', function(){
     this.review = {};
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+    }
   });
 
   var gems = [
