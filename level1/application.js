c481b1598fd316b0f8cc83fda823ec0e74756f9b
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-20 11:57:00 -0700

    use ng-src to load images properly

--- level1/application.js
+++ level1/application.js
@@ -10,14 +10,34 @@
       price: 2,
       description: 'This is description.',
       canPurchase: false,
-      soldOut: 0
+      soldOut: 0,
+      images: [
+        {
+          full: 'image1-full.jpg',
+          thumb: 'image1-thumb.jpg'
+        },
+        {
+          full: 'image2-full.jpg',
+          thumb: 'image2-thumb.jpg'
+        }
+      ];
     },
     {
       name: 'Gem2',
       price: 8.95,
       description: 'This is description2.',
       canPurchase: false,
-      soldOut: 0
+      soldOut: 0,
+      images: [
+        {
+          full: 'image3-full.jpg',
+          thumb: 'image3-thumb.jpg'
+        },
+        {
+          full: 'image4-full.jpg',
+          thumb: 'image4-thumb.jpg'
+        }
+      ];
     }
   ];
 }) ();
