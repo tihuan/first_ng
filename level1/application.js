aae5d6cbf54d6fc768309d3b4d040d545b6dfc3a
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-20 12:41:52 -0700

    Refactor code by extracting tab control from html to a new PanelController

--- level1/application.js
+++ level1/application.js
@@ -3,6 +3,15 @@
   app.controller('StoreController', function() {
     this.products = gems;
   });
+  app.controller('PanelController', function() {
+    this.tab = 1;
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
 
   var gems = [
     {
