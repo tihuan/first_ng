2599e1634c57b3344baea22ab9d1a43acbb3475d
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-20 12:12:52 -0700

    remove ; after images array to make the code work

--- level1/application.js
+++ level1/application.js
@@ -11,16 +11,7 @@
       description: 'This is description.',
       canPurchase: false,
       soldOut: 0,
-      images: [
-        {
-          full: 'image1-full.jpg',
-          thumb: 'image1-thumb.jpg'
-        },
-        {
-          full: 'image2-full.jpg',
-          thumb: 'image2-thumb.jpg'
-        }
-      ];
+      images: []
     },
     {
       name: 'Gem2',
@@ -28,16 +19,7 @@
       description: 'This is description2.',
       canPurchase: false,
       soldOut: 0,
-      images: [
-        {
-          full: 'image3-full.jpg',
-          thumb: 'image3-thumb.jpg'
-        },
-        {
-          full: 'image4-full.jpg',
-          thumb: 'image4-thumb.jpg'
-        }
-      ];
+      images: []
     }
   ];
 }) ();
