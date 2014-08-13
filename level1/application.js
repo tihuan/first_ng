6900c961d25556939be7fa0749b6682501417298
Timmy Huang <timmyhuang0404@gmail.com>
2014-08-12 21:37:45 -0700

    add controller: function and move 'PanelController' into the directive. add controllerAs: 'panels' to remove 'PanelController' as panel in html

--- level1/application.js
+++ level1/application.js
@@ -26,10 +26,26 @@
       this.review = {};
     }
   });
-  app.directive("productDescription", function(){
+  app.directive('productDescription', function(){
     return  {
       restrict: "E",
       templateUrl: "product-title.html"
+    };
+  });
+  app.directive('productPanels', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-panels.html',
+      controller: function() {
+        this.tab = 1;
+        this.selectTab = function(setTab){
+          this.tab = setTab;
+        };
+        this.isSelected = function(checkTab){
+          return this.tab === checkTab;
+        };
+      },
+      controllerAs: 'panel'
     };
   });
 
